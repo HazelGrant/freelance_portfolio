78fb2a5e06012c9799f31592eec4566b77ac90b8
WendyBeth <wendybeth010@gmail.com>
2015-03-22 14:42:06 -0400

    add portfolio style to image photos. add links to layout.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,11 @@
 class Image < ActiveRecord::Base
   belongs_to :portfolio_piece
 
-  has_attached_file :photo, styles: { thumb: "100x100>" }
+  has_attached_file :photo, styles: { portfolio: "580x290>",
+                                      thumb: "100x100>" }
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 
   def embed_url
-    self.photo.url
+    self.photo.url(:portfolio)
   end
 end
